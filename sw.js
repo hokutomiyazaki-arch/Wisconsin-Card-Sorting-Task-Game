507362fd383befa2a8861dcf0fd949d91ec88a4b
Functional Neuro Training <hokutomiyazaki312@gmail.com>
2025-09-04 21:34:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,75 +1,19 @@
-// Service Worker for Wisconsin Card Sorting Task - FNT
-const CACHE_NAME = 'wcst-fnt-v3';
+const CACHE_NAME = 'pwa-test-v1';
 const urlsToCache = [
   './',
-  './index.html',
-  './manifest.json'
+  './index.html'
 ];
 
-// インストール時にキャッシュ
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(err => {
-        console.error('Cache error:', err);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// アクティベート時に古いキャッシュを削除
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      return self.clients.claim();
-    })
-  );
-});
-
-// フェッチ時の処理
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-      .catch(() => {
-        // オフライン時
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      })
-  );
-});
-
-// 古いキャッシュを削除
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
