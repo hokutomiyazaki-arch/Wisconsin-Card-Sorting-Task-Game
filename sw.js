91b0b22c751b11b8ee9ac156f8842b56e49fa148
Functional Neuro Training <hokutomiyazaki312@gmail.com>
2025-09-05 13:50:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,13 @@
-const CACHE_NAME = 'wcst-fnt-v8';
-
-// キャッシュするファイル
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './FNT512.png',
-  './FNT512-transparent.png'
-];
-
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('[ServiceWorker] Caching app shell');
-        return cache.addAll(urlsToCache);
-      })
-  );
+// Simple Service Worker
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// 古いキャッシュを削除
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching', e.request.url);
+  e.respondWith(fetch(e.request));
 });
